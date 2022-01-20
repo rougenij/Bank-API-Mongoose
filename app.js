7da7b51c5e8cd681a44708f33b449f729d95e579
Rouge Nij <rougenij@gmail.com>
2022-01-20 16:37:49 +0200

    heroku push

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api", apiRouter);
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "client/build");
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up on port: ${PORT}`);
 });
