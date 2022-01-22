5f7bbbc66a5a2caed36dcd9f7c6a10eac7f4b911
Rouge Nij <rougenij@gmail.com>
2022-01-22 23:39:21 +0200

    Adding Routes to userStatus

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import NotFound from "./components/NotFound/NotFound";
 import Header from "./components/Header/Header";
 import Users from "./components/Users/Users";
 import Settings from "./components/Settings/Settings";
+import UserStatus from "./components/UserStatus/UserStatus";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Homepage} />
           <Route path="/settings" exact component={Settings} />
+          <Route path="/userstatus" exact component={UserStatus} />
           <Route Path="/users" exact component={Users} />
           <Route component={NotFound} />
         </Switch>
